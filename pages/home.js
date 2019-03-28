08fd946af878bd653e99f608d6674014d3125b19
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-29 06:50:48 +0700

    add some sweets 🍬

---
+++ pages/home.js
@@ -0,0 +1,12 @@
+const home = ({
+  className,
+  ...restProps
+  }) => {
+  return (
+    <div>
+      <h1>This is a homepage</h1>
+    </div>
+  )
+}
+
+export default home
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
+import styles from './index.scss'
+import React from 'react'
+import NoSSR from 'react-no-ssr'
+import cx from 'classnames'
+
 const Home = () => {
+
+  let names = [
+    "Simba",
+    "Masbul",
+    "Cakep",
+    "Cakep Jr.",
+    "Tompel",
+    "Kirana",
+    "Bocil",
+    "Socky",
+    "Kecap",
+    "Tilil",
+    "Betty",
+  ]
+
+  let [title, setTitle] = React.useState(names[ Math.floor(Math.random() * Math.floor(names.length - 1)) ]);
+
+  const randomName = () => {
+    setTitle( names[ Math.floor(Math.random() * Math.floor(names.length - 1)) ] )
+  }
+
   return (
     <div>
-      Hello world from the React SSR!
+      <h1 className={styles.title}>
+        hi, there { title }
+      </h1>
+      <button onClick={randomName}>Random cat</button>
+      <NoSSR>
+        <h2>
+          The title should be red
+        </h2>
+      </NoSSR>
     </div>
   )
 }
