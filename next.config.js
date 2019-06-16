e612bb2ec1e80dcdb58a40792f06522e9da6524d
Ongki Herlambang <meepomodeon@gmail.com>
2019-06-16 21:24:17 +0700

    refine setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
 const compose = require('next-compose')
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
 const withFonts = require('next-fonts')
+const withReactSvg = require('next-react-svg')
+const withImages = require('next-images')
 
 module.exports = compose([
   [withFonts, {
@@ -14,6 +17,10 @@ module.exports = compose([
       importLoaders: 1,
       localIdentName: "[local]___[hash:base64:5]",
     }
+  }],
+  [withImages, {}],
+  [withReactSvg, {
+    include: path.resolve(__dirname, 'src/assets/svg')
   }],
   {
     webpack: (config) => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 
-import './../static/sass/index.scss'
-import 'invoker-layout/grid.module.scss'
-import './../static/fonts/fonts.js'
+import './../src/base.scss'
+import './../src/sass/invoker-layout/grid.module.scss'
+import './../src/assets/fonts/fonts.js'
 
 class CustomApp extends App {
   static async getInitialProps({ Component, ctx }) {
